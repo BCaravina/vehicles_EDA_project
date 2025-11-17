e38497b978d61d1f3495cac0c07050e9bf78d54c
BCaravina <168189345+BCaravina@users.noreply.github.com>
2025-11-17 20:15:47 -0300

    Criando esqueleto do site no app.py

--- app.py
+++ app.py
@@ -2,18 +2,43 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+# TÍTULO E CABEÇALHO
+st.title("Exploração de dados de veículos")
+st.text(
+    "Interaja com a base de dados e os dashboards abaixo para visualizar diversas características sobre veículos à venda nos Estados Unidos no ano de 2019."
+)
+st.divider()
 
-car_data = pd.read_csv("vehicles_us.csv")
-car_data.info()
 
-# CRIANDO CABEÇALHO
+# LENDO, EDITANDO E CARREGANDO OS DADOS NA PÁGINA
+@st.cache_data
+def load_data():
+    """Função para editar o df e remover colunas desnecessárias apenas ao carregar a página para evitar lentidão no site."""
+    df = pd.read_csv("vehicles_us.csv")
+    df = df.drop(columns=["days_listed", "date_posted"])
+    df[["brand", "model_name"]] = df["model"].str.split(pat=" ", n=1, expand=True)
 
-# BOTÃO CRIAR HISTOGRAMA
+    return df
 
-# BOTÃO CRIAR GRÁFICO DISPERSÃO
 
-# [DESAFIO] SUBSTITUIR BOTÕES POR CAIXA DE SELEÇÃO ONDE O USUÁRIO SELECIONA HIST OU DISPERÇÃO
+car_data = load_data()
 
-# ATUALIZAR ARQUIVO READ.ME -> BREVE DESCRIÇÃO DO PROJETO - FEITO OK
+# TABELA DE DADOS
+st.subheader("Base de dados")
+st.write(car_data)
+st.divider()
 
-# CRIAR NOVO SERVIÇO WEB NO RENDER - FEITO OK
+# CAIXA DE SELEÇÃO PARA GERAR HISTOGRAMA (3 opções)
+st.subheader("Histogramas")
+st.write("Selecione uma das opções abaixo para gerar um histograma.")
+st.divider()
+
+# GRÁFICOS DE DISPERSÃO (criar 2 ou 3 e usar um botão para gerar um deles aleatoriamente)
+st.subheader("Gráficos de dispersão")
+st.button("Gerar gráfico")
+st.divider()
+
+# SLIDER BAR COM OS PREÇOS DOS VEÍCULOS
+st.subheader("Barra rolante")
+st.write("Deslize a barra para ver ")
+st.slider()
